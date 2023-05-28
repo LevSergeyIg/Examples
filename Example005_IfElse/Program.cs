10fb0a1a096e6ee2230accf8d3577ae267dc652c
Sergei_Levitin <Welxeor05@gmail.com>
2023-05-28 16:51:23 +0300

    add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет Машенька");
+}
+else
+{
+    Console.Write("Ну привет ");
+    Console.WriteLine(username);
+}
+
+    
